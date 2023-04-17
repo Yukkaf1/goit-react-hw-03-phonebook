631c789f5448be494ac5b19e7d650f69f95dfb32
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-17 17:49:42 +0200

    deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,67 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
+import { Component } from 'react';
+import { Section } from './Section';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+import { ContactList } from './ContactList';
+import { nanoid } from 'nanoid';
 
-import user from '../data/user.json';
-import data from '../data/data.json';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: nanoid(), name: 'Simpson', number: '49-12-512' },
+      { id: nanoid(), name: 'Adams', number: '49-10-000' },
+    ],
+    filter: '',
+  };
 
-export const App = () => {
-  return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics cards={data} />
-    </>
-  );
-};
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const savedNamesList = contacts.map(contact => contact.name);
+
+    if (savedNamesList.includes(name)) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+    }));
+  };
+
+  deleteContact = currId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== currId),
+    }));
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const contactFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(contactFilter)
+    );
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <>
+        <Section headTitle="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter value={this.state.filter} onChange={this.handleChange} />
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+const theme = {
+  colors: {
+    black: 'black',
+    white: 'white',
+    accent: 'tomato',
+    error: 'orangered',
+    success: 'green',
+  },
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
