6a21f952095c52ebeb87096e12f1f55a579ee274
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-17 19:40:42 +0200

    local st

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -11,7 +11,7 @@ export const ContactList = ({ contacts, onDeleteContact }) => {
               {name}: {number}
             </span>
             <Button type="button" onClick={() => onDeleteContact(id)}>
-              delete
+              X
             </Button>
           </СontactItem>
         ))}
